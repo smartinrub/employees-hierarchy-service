d8f6125070beb59d8e35c1571d328d258854b7c5
Sergio Martin Rubio <econsergio@gmail.com>
2020-05-31 16:08:29 +0100

    First commit

---
+++ src/main/kotlin/com/sergiomartinrubio/employeeshierarchyservice/EmployeesHierarchyServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.sergiomartinrubio.employeeshierarchyservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeesHierarchyServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmployeesHierarchyServiceApplication>(*args)
+}
